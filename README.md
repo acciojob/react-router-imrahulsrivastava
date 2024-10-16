95eaa1bfcfa56e02d2f8367d503ff740716dcfd9
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-10-16 10:51:56 +0530

    submission

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React Router
 
-#### Write a program that uses React Router to create two routes: one for the home page and one for an about page. 
+#### Write a program that uses React Router to create two routes: one for the home page and one for an about page.
 
 - The home page should display a welcome message.
 - The about page should display some information about your application.
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import About from "./About.js";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
